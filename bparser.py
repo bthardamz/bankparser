4f8e9f3813193e99e3e4ab58780f37db9676e7b5
bthardamz <95651171+bthardamz@users.noreply.github.com>
2021-12-11 00:19:46 +0100

    Update and rename tempelhof.py to bparser.py

--- bparser.py
+++ bparser.py
@@ -3,6 +3,10 @@ import pandas as pd
 excel = pd.read_excel('trs.xlsx')
 excel.to_csv('utgifter.csv', index=None, header=True)
 '''
+
+'''source file is in xls whereas pandas requires xlsx, sigh'''
+
+
 lines=list()
 with open('utgifter.csv', 'r') as f:
 	for idx, line in enumerate(f.readlines()):
